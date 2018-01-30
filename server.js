c7eb5219e0fbcd31fd45c1a80890c96a23d84110
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-01-30 09:53:22 -0600

    indented server like i want, updaed readme, npm init and install

--- server.js
+++ server.js
@@ -7,21 +7,21 @@ app.set('port', port)
 const request = require('request');
 
 // MIDDLEWARE (transform stream)
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 
 app.get('/api/itunes/*', (req, res) => {
-  let apiCall = req.url.slice('/api/itunes/'.length)
-  let apiReq = `https://itunes.apple.com/search${apiCall}`
-  request.get(apiReq, (err, _, body) => {
-    res.send(body)
-  });
+    let apiCall = req.url.slice('/api/itunes/'.length)
+    let apiReq = `https://itunes.apple.com/search${apiCall}`
+    request.get(apiReq, (err, _, body) => {
+        res.send(body)
+    });
 });
 
 app.listen(port, () =>
-  console.log(`Listening on port: ${port}`)
+    console.log(`Listening on port: ${port}`)
 )
